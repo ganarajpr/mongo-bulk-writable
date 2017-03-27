c63832c5c36428ed16cc35f5b4ce20dd073931ca
Ganaraj P R <ganarajpr@gmail.com>
2017-03-27 16:32:54 +0100

    update to expose bulk

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mongo-bulk-writable
-expose mongodb BulkOp as a writable stream
+Expose mongodb BulkOp as a writable stream
 
 ## Install 
 
@@ -12,9 +12,12 @@ Simple use case :
 ```js
 var BulkWritable = require('mongo-bulk-writable');
 var col; // get a collection object from driver
-var writable = new BulkWritable(col.initializeOrderedBulkOp(), function write(chunk, next) {
-  this.bulk.insert(chunk);
-  next();
+var writable = new BulkWritable( {
+    collection :col,
+    callback: function write(bulk, chunk, next) {
+      bulk.insert(chunk);
+      next();
+    }
 });
 // pipe it
 req.pipe(writable);
@@ -25,9 +28,16 @@ Or
 ```js
 var BulkWritable = require('mongo-bulk-writable');
 var col; // get a collection object from driver
-var writable = new BulkWritable(col.initializeUnorderedBulkOp(), function write(chunk, next) {
-  this.bulk.find( { status: "P" } ).update( { $set: { comment: chunk.comment} } );
-  next();
+var writable = new BulkWritable( {
+  collection: col,
+  ordered: true,
+  callback: function write(bulk, chunk, next) {
+    bulk
+      .find( { status: "P" } )
+      .update( { $set: { comment: chunk.comment} } );
+    next();
+  },
+  highWaterMark: 10000
 });
 // pipe it
 req.pipe(writable);
--- test/index.test.js
+++ test/index.test.js
@@ -3,89 +3,101 @@
 
 var MongoClient = require('mongodb').MongoClient;
 var es = require('event-stream');
-var bulkWritable = require('../lib/index');
+var MongoBulkWriteStream = require('../lib/index');
+var arr = require('./sample.js');
+var expect = require('chai').expect;
 
 // Connection URL
 var url = 'mongodb://localhost:27017/myproject';
 
 var db, col;
+describe('Simple Case', () => {
 
-module.exports = {
-    setUp: function (done) {
-
-        MongoClient.connect(url, function (err, mongo) {
-            if (err) {
+    beforeEach((done) => {
+        MongoClient.connect(url)
+            .then((mongo) => {
+                db = mongo;
+                col = db.collection('test');
+            })
+            .then( () => {
+                col.drop();
+            })
+            .catch( (err) => {
                 console.log(err);
-            }
-            db = mongo;
-            col = db.collection('test' + (Date.now() % 10));
-            done();
-        });
-    },
-    testSimpleCase: function (test) {
-        var arr = require('./sample.js');
-        var reader = es.readArray(arr), bulk = col.initializeOrderedBulkOp();
-        var writable = bulkWritable(bulk, function write(chunk, next) {
-            this.bulk.insert(chunk);
-            next();
+            })
+            .then( () => {
+                done();
+            })
+    });
+
+    afterEach(() => {
+        if (db) {
+            db.close();
+        }
+    })
+    it('should insert 21 records with pipe', (done) => {
+        var reader = es.readArray(arr);
+        var writable = new MongoBulkWriteStream({
+            callback:  function write(bulk, chunk, next) {
+                bulk.insert(chunk);
+                next();
+            },
+            collection: col
         });
         writable.on('finish', function () {
             col.find({}).toArray(function (err, res) {
-                test.equal(res.length, 21);
-                test.done();
+                expect(res).to.have.lengthOf(21);
+                done();
             });
         });
         writable.on('error', function (err) {
             console.log(err);
-            test.done(err);
+            done(err);
         });
         reader.pipe(writable);
-    },
-    testWrite: function (test) {
-        var arr = require('./sample.js');
-        var reader = es.readArray(arr), bulk = col.initializeOrderedBulkOp();
-        var writable = bulkWritable(bulk, function write(chunk, next) {
-            this.bulk.insert(chunk);
-            next();
-        });
+    });
 
+    it('should insert 21 records with write', (done) => {
+        var reader = es.readArray(arr);
+        var writable = new MongoBulkWriteStream({
+            callback:  function write(bulk, chunk, next) {
+                bulk.insert(chunk);
+                next();
+            },
+            collection: col
+        });
         writable.on('error', function (err) {
-            console.log(err);
-            test.done(err);
+            done(err);
         });
-
-        //reader.pipe(writable);
         arr.forEach(function (el) {
             writable.write(el);
         });
 
         writable.end(function () {
             col.find({}).toArray(function (err, res) {
-                test.equal(res.length, 21);
-                test.done();
+                expect(res).to.have.lengthOf(21);
+                done();
             });
         });
-    },
-    testEmptyBulk: function (test) {
-        var reader = es.readArray([]), bulk = col.initializeOrderedBulkOp();
-        var writable = bulkWritable(bulk, function write(chunk, next) {
-            this.bulk.insert(chunk);
-            next();
+    });
+
+    it('should insert empty records without issue', (done) => {
+        var reader = es.readArray([]);
+        var writable = new MongoBulkWriteStream({
+            callback:  function write(bulk, chunk, next) {
+                bulk.insert(chunk);
+                next();
+            },
+            collection: col
         });
         writable.on('finish', function () {
-            test.done();
+            done();
         });
         writable.on('error', function (err) {
-            console.log(err);
-            test.done(err);
+            done(err);
         });
         reader.pipe(writable);
-    },
-    tearDown: function (done) {
-        if (db) {
-            col.drop();
-            db.close();
-        }
-        done();
-    }
-};
+    });
+
+    
+});
